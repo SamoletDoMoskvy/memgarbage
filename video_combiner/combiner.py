bb59040775d9b22297d439731484aebc4871489c
sdm <aleksandr.tugay.02@gmail.com>
2021-10-11 12:26:57 +0300

    Добавлена проверка already_used в combiner

--- video_combiner/combiner.py
+++ video_combiner/combiner.py
@@ -6,7 +6,6 @@ from moviepy.editor import *
 import datetime
 import pathlib
 import time
-import re
 
 
 def comb():
@@ -25,26 +24,29 @@ def comb():
                 for video_path in folder.glob("*"):
                     # Кидаем в БД метку об использовании видео
                     document_id = video_path.stem
-                    Video.objects.filter(document_id=document_id).update(already_used=True)
-                    try:
-                        video = VideoFileClip(str(video_path))
-                    except OSError:
+                    if Video.objects.get(document_id=document_id).already_used:
                         continue
+                    else:
+                        Video.objects.filter(document_id=document_id).update(already_used=True)
+                        try:
+                            video = VideoFileClip(str(video_path))
+                        except OSError:
+                            continue
 
-                    if counter + 1 >= settings.VIDEOS_QUANTITY:
-                        # в данном случае видео готово и его нужно сохранить
-                        result.write_videofile(str(generated_folder / f"{datetime.datetime.now()}.mp4"))
-                        result = None
-                        counter = 0
-                        continue
+                        if counter + 1 >= settings.VIDEOS_QUANTITY:
+                            # в данном случае видео готово и его нужно сохранить
+                            result.write_videofile(str(generated_folder / f"{datetime.datetime.now()}.mp4"))
+                            result = None
+                            counter = 0
+                            continue
 
-                    if not result:
-                        result = concatenate_videoclips([video, shum], method="compose")
-                        counter += 1
+                        if not result:
+                            result = concatenate_videoclips([video, shum], method="compose")
+                            counter += 1
 
-                    elif result:
-                        result = concatenate_videoclips([result, video, shum], method="compose")
-                        counter += 1
+                        elif result:
+                            result = concatenate_videoclips([result, video, shum], method="compose")
+                            counter += 1
 
         else:
             time.sleep(5)
